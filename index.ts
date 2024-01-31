65622a7d72d3ed8977ac5b6e775dd02734c33e42
jafrex <133799081+jafrex001@users.noreply.github.com>
2024-01-31 20:12:24 +0500

    -Set up the config and basic files

---
+++ index.ts
@@ -0,0 +1,3 @@
+// An ATM Machine using Typescript accessed through the command line. Use the inquirer package to take userinput through the cli. The user can register an account and also login using a name and a pin. Account balance will be generated randomly. User can also withdraw and deposit money. 
+
+//Use the chalk library after the main project is complete to make the project look better.
