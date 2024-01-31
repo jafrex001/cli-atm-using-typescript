f068f476f704c39e09eb8e36474312489adfbfe7
jafrex <133799081+jafrex001@users.noreply.github.com>
2024-01-31 23:38:56 +0500

    -Added basic logic for base choices

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // An ATM Machine using Typescript accessed through the command line. Use the inquirer package to take userinput through the cli. The user can register an account and also login using a name and a pin. Account balance will be generated randomly. User can also withdraw and deposit money. 
-export {};
 //Use the chalk library after the main project is complete to make the project look better.
+import inquirer from "inquirer";
+let mainMenuInput = await inquirer.prompt([
+    {
+        name: 'mainOptions',
+        type: 'list',
+        choices: ['Register', 'Login']
+    }
+]);
+if (mainMenuInput.mainOptions === 'Register') {
+    console.log("Register working");
+}
+else if (mainMenuInput.mainOptions === 'Login') {
+    console.log("Login working");
+}
